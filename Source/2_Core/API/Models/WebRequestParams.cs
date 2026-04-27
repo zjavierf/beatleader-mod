a8db490ebea9724ea2cfba4d14d503f8dc63758c
NSGolova <golova@golova.dev>
2026-04-27 14:10:59 +0000

    Increased network timeouts to something more forgiving

--- Source/2_Core/API/Models/WebRequestParams.cs
+++ Source/2_Core/API/Models/WebRequestParams.cs
@@ -1,11 +1,11 @@
-using System;
+﻿using System;
 using System.Net.Http;
 
 namespace BeatLeader.WebRequests {
     public class WebRequestParams {
         public float UploadTrackingPrecision { get; set; } = 100f;
         public float DownloadTrackingPrecision { get; set; } = 100f;
-        public int TimeoutSeconds { get; set; } = 30;
+        public int TimeoutSeconds { get; set; } = 120;
         public int RetryCount { get; set; } = 0;
         public HttpCompletionOption ResponseCompletionOption { get; set; } = HttpCompletionOption.ResponseHeadersRead;
 
